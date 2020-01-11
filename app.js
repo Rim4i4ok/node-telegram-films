a8e554e25d2505e76cbb048db780665f5f3ba2d4
Rim4i4ok <rim4i4ok@gmail.com>
2020-01-12 02:20:04 +0300

    info for film

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
 const TelegramBot = require("node-telegram-bot-api");
-const config = require("config");
+const request = require("request");
 
-const token = config.get("botToken");
+const { botToken, movieApiUrl } = require("./settings");
+
+const token = botToken;
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
+// Matches "/movie [whatever]"
+bot.onText(/\/movie (.+)/, (msg, match) => {
 	// 'msg' is the received Message from Telegram
 	// 'match' is the result of executing the regexp above on the text content
 	// of the message
 
 	const chatId = msg.chat.id;
-	const resp = match[1]; // the captured "whatever"
 
-	// send back the matched "whatever" to the chat
-	bot.sendMessage(chatId, resp);
+	const url = `${movieApiUrl}&t=${movie}`;
+
+	request(url, function(error, response, body) {
+		if (!error && response.statusCode === 200) {
+			bot
+				.sendMessage(chatId, `Loading info for ${movie} ...`, {
+					parse_mode: "Markdown",
+				})
+				.then(() => {
+					bot.sendMessage(chatId, `Info:\n ${body}`);
+				});
+		}
+	});
 });
-
-// Listen for any kind of message. There are different kinds of
-// messages.
-// bot.on("message", msg => {
-// });
